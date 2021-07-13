2c2d19f2aeda70d2eb692071929dc2877801d217
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-07-13 16:29:49 -0700

    cleaned up template code and initial file structure

---
+++ src/Joke.js
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+
+class Joke extends Component {
+  render() {
+    return(
+      <div className="Joke">
+
+      </div>
+    )
+  }
+}
+
+export default Joke;
