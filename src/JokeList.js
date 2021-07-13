4e1d5a658065f0e5e7089ba22b5d6a682f52ef01
CARLY DEKOCK <CARLY.M.DEKOCK@GMAIL.COM>
2021-07-13 16:41:15 -0700

    calling api successfully, returing 10 jokes

--- src/JokeList.js
+++ src/JokeList.js
@@ -1,10 +1,33 @@
 import React, {Component} from 'react';
+import axios from 'axios';
 
 class JokeList extends Component {
+  static defaultProps = {
+    numJokesToGet: 10
+  };
+
+  constructor(props){
+    super(props);
+    this.state = { jokes: [] };
+  }
+  async componentDidMount(){
+    let jokes = [];
+    while(jokes.length < this.props.numJokesToGet){
+      let res = await axios.get('https://icanhazdadjoke.com/', {headers: {Accept: 'application/json'}});
+      jokes.push(res.data.joke);
+    }
+    // console.log(jokes);
+    this.setState({jokes: jokes})
+  }
   render() {
     return (
       <div className="JokeList">
         <h1>Dad Jokes</h1>
+        <div className="JokeList-jokes">
+          {this.state.jokes.map(joke => (
+            <div>{joke}</div>
+          ))}
+        </div>
       </div>
     )
   }
